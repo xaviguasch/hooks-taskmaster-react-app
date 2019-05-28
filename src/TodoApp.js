85e9091a0dd9a8601a0a0b0707a88d97714bd489
xaviguasch <xaviguasch@gmail.com>
2019-05-28 19:17:38 +0200

    added Form with hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TodoList from './TodoList'
+import TodoForm from './TodoForm'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import AppBar from '@material-ui/core/AppBar'
@@ -13,6 +14,10 @@ function TodoApp() {
     { id: 3, task: 'Grow Beard', completed: false }
   ]
   const [todos, setTodos] = useState(initialTodos)
+
+  const addTodo = newTodoText => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }])
+  }
 
   return (
     <Paper
@@ -29,6 +34,7 @@ function TodoApp() {
           <Typography color='inherit'>TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
     </Paper>
   )
